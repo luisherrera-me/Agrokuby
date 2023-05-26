93f583b1cc1503bead7bfbfb39571a0a27f6399f
arfindev <developerarfin@gmail.com>
2023-05-27 00:32:26 +0600

    broken logout button fixed

--- app/src/main/java/com/kubyapp/agrokuby/presentation/home_screen/navigationbar/bar_view/NavegacionHost.kt
+++ app/src/main/java/com/kubyapp/agrokuby/presentation/home_screen/navigationbar/bar_view/NavegacionHost.kt
@@ -5,24 +5,30 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.kubyapp.agrokuby.navigation.Screens
 
 import com.kubyapp.agrokuby.presentation.home_screen.navigationbar.Items_menu
 import com.kubyapp.agrokuby.presentation.home_screen.navigationbar.bar_view.psychiatry_screen.Psychiatry
 import com.kubyapp.agrokuby.presentation.home_screen.navigationbar.bar_view.starting_screen.startingScreen
+import com.kubyapp.agrokuby.presentation.login_screen.SignInScreen
 
 @Composable
-fun NavegacionHost(navController: NavController){
-    NavHost(navController = navController as NavHostController,
+fun NavegacionHost(navController: NavController) {
+    NavHost(
+        navController = navController as NavHostController,
         startDestination = Items_menu.Pantallas1.ruta
-        ){
-        composable(Items_menu.Pantallas1.ruta){
+    ) {
+        composable(Items_menu.Pantallas1.ruta) {
             settings()
         }
-        composable(Items_menu.Pantallas2.ruta){
+        composable(Items_menu.Pantallas2.ruta) {
             startingScreen(navController = navController)
         }
-        composable(Items_menu.Pantallas3.ruta){
+        composable(Items_menu.Pantallas3.ruta) {
             Psychiatry()
+        }
+        composable(route = Screens.SignInScreen.route) {
+            SignInScreen(navController = navController)
         }
 
     }
